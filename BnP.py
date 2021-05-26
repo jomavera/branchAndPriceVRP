8eeeaca598a8584b0e8c50e934599486b53eff70
jomavera <josemanuel.vera.aray@gmail.com>
2021-05-26 17:14:29 -0500

    Add the best relax solution as upper bound to objetive value of nodes in order to be added to queue

--- BnP.py
+++ BnP.py
@@ -31,16 +31,16 @@ def branch_n_price(n, demands, capacity, distances, MasterProb):
         branch_routes = MP_branch.modelo.getA().toarray()
         sol_is_int = all([float(round(s,4)).is_integer() for s in solution ])
         # sol_is_int = all([False if i > 0.3 and np.abs(i - 1.0) > 0.3 else True for i in solution ])
+
         if obj_val < best_int_obj and sol_is_int:
             print(f"Best Integer Obj: {obj_val}")
             print(f"Nodes explored: {nodes_explored}")
             best_int_obj = obj_val
-            # if sol_is_int:
 
             # print(f"best sol: {solution}")
             best_model = copy_model(branch_cost, branch_routes, MP_branch)
         
-        elif obj_val < best_relax_obj:
+        if obj_val < best_relax_obj:
             print(f"Best Relaxed Obj: {obj_val}")
             print(f"Nodes explored: {nodes_explored}")
             best_relax_obj = obj_val
@@ -48,11 +48,12 @@ def branch_n_price(n, demands, capacity, distances, MasterProb):
         # --- # --- # Column generation # --- # --- #
         new_MP = column_generation(n, demands, capacity, distances, duals, MP_branch)
 
-        if new_MP != None:
+        if new_MP != None :
             new_MP.RelaxOptimize()
             branch_cost = new_MP.getCosts()
             branch_routes = new_MP.modelo.getA().toarray()
-            queue.insert(new_MP.relax_modelo.ObjVal, copy_model(branch_cost, branch_routes, new_MP))
+            if new_MP.relax_modelo.ObjVal <= best_relax_obj:
+                queue.insert(new_MP.relax_modelo.ObjVal, copy_model(branch_cost, branch_routes, new_MP))
         
         else:
             # --- # If stopped col generation then branch if solution is not integer # --- #
@@ -61,7 +62,7 @@ def branch_n_price(n, demands, capacity, distances, MasterProb):
 
                 # print("#--#--#--# Not integer solution  ........Branching")
                 queue = branch(
-                    branch_cost, branch_routes, n, demands, capacity, distances, duals, solution, MP_branch, queue, best_int_obj
+                    branch_cost, branch_routes, n, demands, capacity, distances, duals, solution, MP_branch, queue, best_relax_obj
                 )
             else:
                 # print(f"best sol: {solution}")
@@ -70,7 +71,7 @@ def branch_n_price(n, demands, capacity, distances, MasterProb):
     return best_model
 
 
-def branch(branch_cost, branch_routes, n, demands, capacity, distances, duals, solution_to_branch, MP_to_copy, queue, best_int_obj):
+def branch(branch_cost, branch_routes, n, demands, capacity, distances, duals, solution_to_branch, MP_to_copy, queue, best_inc_obj):
 
     frac_ixs = []
 
@@ -111,8 +112,12 @@ def branch(branch_cost, branch_routes, n, demands, capacity, distances, duals, s
             for j in locations_prime:
 
                 if (s1_and_s2[i - 1] and s1_not_s2[j - 1]):
-                    SP_1.modelo.addConstr(SP_1.y[i - 1] + SP_1.y[j - 1] == 2)
-                    SP_2.modelo.addConstr(SP_2.y[i - 1] + SP_2.y[j - 1] == 1)
+                    # SP_1.modelo.addConstr(SP_1.y[i - 1] + SP_1.y[j - 1] == 2)
+                    # SP_2.modelo.addConstr(SP_2.y[i - 1] + SP_2.y[j - 1] == 1)
+                    SP_1.modelo.addConstr(SP_1.y[i - 1] == 1)
+                    SP_1.modelo.addConstr(SP_1.y[j - 1] == 1)
+                    SP_2.modelo.addConstr(SP_2.y[i - 1] == 1)
+                    SP_2.modelo.addConstr(SP_2.y[j - 1] == 0)
 
 
         MP_1, MP_2 = copy_models(branch_cost, branch_routes, MP_to_copy)
@@ -132,7 +137,7 @@ def branch(branch_cost, branch_routes, n, demands, capacity, distances, duals, s
                 MP_1.RelaxOptimize()
                 mp1_cost = MP_1.getCosts()
                 mp1_routes = MP_1.modelo.getA().toarray()
-                if MP_1.relax_modelo.ObjVal < best_int_obj:
+                if MP_1.relax_modelo.ObjVal <= best_inc_obj:
                     queue.insert(MP_1.relax_modelo.ObjVal, copy_model(mp1_cost, mp1_routes, MP_1))
 
         SP_2.modelo.update()
@@ -150,7 +155,7 @@ def branch(branch_cost, branch_routes, n, demands, capacity, distances, duals, s
                 MP_2.RelaxOptimize()
                 mp2_cost = MP_2.getCosts()
                 mp2_routes = MP_2.modelo.getA().toarray()
-                if MP_2.relax_modelo.ObjVal < best_int_obj:
+                if MP_2.relax_modelo.ObjVal <= best_inc_obj:
                     queue.insert(MP_2.relax_modelo.ObjVal, copy_model(mp2_cost, mp2_routes, MP_2))
 
     return queue
